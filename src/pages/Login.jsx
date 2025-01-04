b9fb9572ecae025426f0646b5a91f340d12aac17
luthando496 <luthandodidiza197@gmail.com>
2025-01-03 16:13:20 -0800

    login starter

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import { supabase } from '../superclient';
+
+
 
 
 
 const Login = () => {
+    const [userInfo,setUser] = useState({
+        email:"",
+        password:''
+    })
+
+    const {email,password} = userInfo;
+
+
+    const onChangeInputs = (e) =>{
+
+        setUser((prev)=>({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    const signInWithEmail = async() => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email: email,
+          password
+        })
+      }
+
+
+    //   useEffect(()=>{
+
+    //   })
+      
   return (
     <>
     <Navbar />
@@ -15,21 +47,21 @@ const Login = () => {
 
                 <div className="flex flex-col gap-5">
                     <label htmlFor="" className="text-xl text-medium text-orange-600">Email: </label>
-                    <input type="text" placeholder='Enter Your Email' name="email" id="" className="px-10 py-2 rounded-xl placeholder:text-orange-600  focus:outline-none border focus:border-orange-600" />
+                    <input type="text" value={email} onChange={onChangeInputs} placeholder='Enter Your Email' name="email" id="" className="px-10 py-2 rounded-xl placeholder:text-orange-600  focus:outline-none border focus:border-orange-600" />
                 </div>
 
                 <div className="flex flex-col gap-5">
                     <label htmlFor="" className="text-xl text-medium text-orange-600">Password: </label>
-                    <input type="text" placeholder='Enter Your Password' name="password" id="" className="px-10 py-2 rounded-xl placeholder:text-orange-600  focus:outline-none border focus:border-orange-600" />
+                    <input value={password} onChange={onChangeInputs} type="text" placeholder='Enter Your Password' name="password" id="" className="px-10 py-2 rounded-xl placeholder:text-orange-600  focus:outline-none border focus:border-orange-600" />
                 </div>
                 <div className="flex justify-center gap-5">
-                    <button className="bg-white font-bold uppercase tracking-[2px] duration-300 hover:translate-y-1 text-teal-700 hover:-translate-x-[2px] px-6 py-3 rounded-xl shadow-sm shadow-orange-900">Sign In</button>
+                    <button onClick={signInWithEmail} className="bg-white font-bold uppercase tracking-[2px] duration-300 hover:translate-y-1 text-teal-700 hover:-translate-x-[2px] px-6 py-3 rounded-xl shadow-sm shadow-orange-900">Sign In</button>
                 </div>
 
 
-                <div className="flex justify-center gap-5">
+                {/* <div className="flex justify-center gap-5">
                     <button className="bg-pink-600 font-bold uppercase tracking-[2px] duration-300 hover:translate-y-1 text-white hover:-translate-x-[2px] px-16 py-3 rounded-3xl shadow-sm shadow-orange-900">Google</button>
-                </div>
+                </div> */}
             </div>
         </div>    
     
